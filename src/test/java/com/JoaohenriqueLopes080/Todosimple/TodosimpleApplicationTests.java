bda32bf83cc5e7bbb8d7efbbe0f5ce8e2e946ae0
Joao <kawaseika@gmail.com>
2023-09-07 19:24:39 -0300

    Spring Boot Project

---
+++ src/test/java/com/JoaohenriqueLopes080/Todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JoaohenriqueLopes080.Todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
